38d5f93926aa2fb4ea896fdce2ecf57190e07366
felixfavour <felixfavour0@gmail.com>
2023-07-28 02:23:54 +0100

    chore: converted extension to HTML + many more

--- worker.js
+++ worker.js
@@ -4,18 +4,21 @@ chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONT
 
 chrome.cookies.get({ url: 'https://my.bible.com', name: 'YouVersionToken2' }, (cookie) => {
   if (cookie) {
-    chrome.storage.local.set({ YouVersionToken2: cookie.value }).then(() => {
-      console.log("Value is set");
-    });
+    chrome.storage.local.set({ YouVersionToken2: cookie.value })
     console.log('YouVersionToken2', cookie.value);
+  }
+});
+
+chrome.cookies.get({ url: 'https://my.bible.com', name: 'yvid' }, (cookie) => {
+  if (cookie) {
+    chrome.storage.local.set({ yvid: cookie.value })
+    console.log('yvid', cookie.value);
   }
 });
 
 chrome.cookies.get({ url: 'https://my.bible.com', name: 'OAUTH' }, (cookie) => {
   if (cookie) {
-    chrome.storage.local.set({ OAUTH: cookie.value }).then(() => {
-      console.log("Value is set");
-    });
+    chrome.storage.local.set({ OAUTH: cookie.value })
     console.log('OAUTH', cookie.value);
   }
 });
